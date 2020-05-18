3f2bc1fac85ba05f16c63380308122ff7e5c7a13
MasqueradexX <linzhikai00@gmail.com>
2020-05-18 01:12:59 +0000

    ..

--- bomb-baby/solution.py
+++ bomb-baby/solution.py
@@ -1,2 +1,18 @@
 def solution(x,y):
-    pass
+    x = int(x)
+    y = int(y)
+    count = 0
+    while (x,y) != (1,1):
+        if x == 0 or y == 0:
+            return 'impossible'
+        if x > y:
+            step = max([x/y-1,1])
+            x -= step*y
+            count += step
+        else:
+            step = max([y/x-1,1])
+            y -= step*x
+            count += step
+    return str(count)
+
+print(solution('4', '7'))
